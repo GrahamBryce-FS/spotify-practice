05f2191f94850a39e171025da8f86605dab9f16c
Bryce Graham <bgraham1@student.fullsail.edu>
2023-07-19 16:54:19 +0900

    working album and artist feature

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-const spotController = require('../controllers/spotController')
+// const spotController = require('../controllers/spotController')
 
 router.get('/login', authController.authorize);
 router.get('/callback', authController.callback);
-router.get('/search', spotController.searchHandler);
+router.get('/search', authController.search);
+router.get('/artist', authController.artistSearch);
+router.get('/album', authController.albumSearch);
+
 
 module.exports = router;
 
